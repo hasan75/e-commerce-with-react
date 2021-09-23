a7f79cf710ebde090e32a406cd87a1d4ba38b6de
Hasan75 <hasanjab14@gmail.com>
2021-09-23 19:39:04 +0600

    nav bar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from '../../images/logo.png'
+import logo from '../../images/logo.png';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <div className="header">
             <h2>Header</h2>
-            <img src={logo} alt="img-logo" srcset="" />
+            <img className="logo" src={logo} alt="img-logo" srcset="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
