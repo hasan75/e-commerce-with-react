b6f62943a101e2b5bf58bfdc74984432afc2f186
Hasan75 <hasanjab14@gmail.com>
2021-09-23 19:55:29 +0600

    shop section added and API called

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <h2>Header</h2>
             <img className="logo" src={logo} alt="img-logo" srcset="" />
             <nav>
                 <a href="/shop">Shop</a>
